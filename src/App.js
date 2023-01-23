da75473cd83683266625e9b3156c9f3da1e78a53
Elin Amrén <elin.amren@adeprimo.se>
2023-01-23 14:55:38 +0100

    clean upp and installed sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Nånting?
       </header>
+      <section>
+        <h1>Matsedel</h1>
+        <ul>
+          <li>Köttfärssås</li>
+          <li>Kikärtsgryta</li>
+          <li>Potatisbullar</li>
+          <li>Korv med bröd</li>
+        </ul>
+      </section>
     </div>
   );
 }
